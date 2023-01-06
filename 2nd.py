95bce99a608601eba8c9adea6887e75acb91fd8b
pooja-ck <pooja.ck@asmltd.com>
2023-01-06 15:28:29 +0530

    2nd

---
+++ 2nd.py
@@ -0,0 +1,9 @@
+x = input("enter any number")
+b = input("what you want to do with this number?")
+if b == "add":
+    print("ok cool")
+else:
+    print("get lost!")
+c = input("then enter the second number")
+z = x+c
+print("here is ur result", z,    "tadaaaa!")
